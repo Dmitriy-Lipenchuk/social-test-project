04517726ced38237eccc1c723a4bb3f7d36a7544
Admin <laksdkjh@mail.ru>
2022-07-18 21:30:42 +0300

    Swagger_Diana

--- src/main/java/com/example/socialkata/webapp/config/SwaggerConfig.java
+++ src/main/java/com/example/socialkata/webapp/config/SwaggerConfig.java
@@ -7,13 +7,12 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
+    public Docket swaggerApiConfig() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -21,35 +20,5 @@ public class SwaggerConfig {
                 .build();
 
     }
+
 }
-//package com.example.socialkata.webapp.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//import com.google.common.base.Predicates;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig extends WebMvcConfigurerAdapter
-//{
-//    @Bean
-//    public Docket api() {
-//
-//        return new Docket(DocumentationType.SWAGGER_2).select()
-//                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-//                .build();
-//    }
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry)
-//    {
-//        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
-//}
