5921ef709d7aaa5fd843401f7c9df8d37f60fa4c
Listanuv <solonovi9@gmail.com>
2022-07-23 10:19:59 +0300

    Сделал красивее файл инициализации БД Добавил метод в UserDao поиска по email Добавил сервисный метод для UserDetails

--- src/main/java/com/example/socialkata/dao/impl/model/UserDaoImpl.java
+++ src/main/java/com/example/socialkata/dao/impl/model/UserDaoImpl.java
@@ -3,9 +3,15 @@ package com.example.socialkata.dao.impl.model;
 import com.example.socialkata.dao.abstracts.model.UserDao;
 import com.example.socialkata.dao.impl.GenericDaoAbstract;
 import com.example.socialkata.model.entity.user.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDaoImpl extends GenericDaoAbstract<User, Long> implements UserDao {
-
+    public UserDetails getByEmail(String username){
+       return (UserDetails) entityManager.createQuery("select a from User as a WHERE a.email=:emailParam")
+                .setParameter("emailParam",username)
+                .getSingleResult();
+    }
 }
