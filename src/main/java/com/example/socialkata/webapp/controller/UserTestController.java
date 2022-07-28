384b1d7d466ea7514026d2f4650c4263bb2cb65b
martynovov@yahoo.com <martynovov@yahoo.com>
2022-07-28 14:18:11 +0300

    v2.2 used MockMvc

--- src/main/java/com/example/socialkata/webapp/controller/UserTestController.java
+++ src/main/java/com/example/socialkata/webapp/controller/UserTestController.java
@@ -3,9 +3,11 @@ package com.example.socialkata.webapp.controller;
 import com.example.socialkata.model.entity.user.User;
 import com.example.socialkata.service.impl.model.UserServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 //@RequestMapping("/")
@@ -20,5 +22,11 @@ public class UserTestController {
     @GetMapping("/usersList")
     public List<User> getAllUsers() {
         return userService.getAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> getUserById(@PathVariable("id") long id) {
+        Optional<User> user = userService.getById(id);
+        return user;
     }
 }
--- src/test/java/com/example/socialkata/UserControllerTest.java
+++ src/test/java/com/example/socialkata/UserControllerTest.java
@@ -1,28 +1,21 @@
 package com.example.socialkata;
 
-import com.example.socialkata.model.entity.user.User;
 import com.example.socialkata.webapp.controller.UserTestController;
-import com.github.database.rider.core.api.configuration.DBUnit;
-import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.connection.ConnectionHolder;
 import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.core.api.dataset.SeedStrategy;
-import com.github.database.rider.junit5.DBUnitExtension;
-import com.github.database.rider.junit5.api.DBRider;
 import com.github.database.rider.junit5.util.EntityManagerProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-/**
- * comments
- */
 public class UserControllerTest extends AbstractTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private UserTestController userTestController;
@@ -37,27 +30,15 @@ public class UserControllerTest extends AbstractTest {
 
     @Test
     @DataSet(value = "users.xml", cleanAfter = true)
-    public void shouldListUsers() {
-        assertThat(userTestController.getAllUsers()).isNotNull();
-        assertThat(userTestController.getAllUsers().size()).isEqualTo(10);
-    }
+    public void getByIdTest() throws Exception {
 
-//    @Test
-//    @ExpectedDataSet("empty.xml")
-//    public void shouldDeleteUser() {
-//        assertThat(userTestController).isNotNull();
-//        assertThat(userTestController.getAllUsers().size()).isEqualTo(0);
-//        userTestController.getById(2L).ifPresent(userTestController::delete);
-//        //assertThat(userRepository.count()).isEqualTo(2); //assertion is made by @ExpectedDataset
-//    }
-//
-//    @Test
-//    @DataSet(cleanBefore = true)//as we didn't declared a dataset DBUnit wont clear the table
-//    @ExpectedDataSet("expected-users.xml")
-//    public void shouldInsertUser() {
-//        assertThat(userTestController).isNotNull();
-//        assertThat(userTestController.getAllUsers().size()).isEqualTo(0);
-//        userTestController.create(new User());
-//        //assertThat(userRepository.count()).isEqualTo(1); //assertion is made by @ExpectedDataset
-//    }
+        long id = 5;
+
+        mockMvc.perform(
+                get("/users/{id}", id))
+//                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(id))
+                .andExpect(jsonPath("$.email").value("d4444@gmail.com"));
+
+    }
 }
