073bb55272bdd41a14b1acbbaf0c27b416963a49
martynovov@yahoo.com <martynovov@yahoo.com>
2022-07-28 23:01:15 +0300

    v2.4 add application-test.properties

--- src/test/java/com/example/socialkata/UserControllerTest.java
+++ src/test/java/com/example/socialkata/UserControllerTest.java
@@ -1,16 +1,11 @@
 package com.example.socialkata;
 
-import com.github.database.rider.core.api.connection.ConnectionHolder;
 import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.junit5.util.EntityManagerProvider;
 import org.junit.jupiter.api.Test;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 class UserControllerTest extends AbstractTest {
-
-    private ConnectionHolder connectionHolder = () ->
-            EntityManagerProvider.instance("junit5-pu").clear().connection();
 
     @Test
     @DataSet(value = "users.xml, role.xml, active.xml", cleanAfter = true)
