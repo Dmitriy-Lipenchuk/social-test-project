36eb28efa66364f6edf8c3a4d8aec1cc148e6cdc
Admin <laksdkjh@mail.ru>
2022-08-01 22:07:59 +0300

    UserController

--- src/main/java/com/example/socialkata/SocialKataApplication.java
+++ src/main/java/com/example/socialkata/SocialKataApplication.java
@@ -2,8 +2,8 @@ package com.example.socialkata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class SocialKataApplication {
 
