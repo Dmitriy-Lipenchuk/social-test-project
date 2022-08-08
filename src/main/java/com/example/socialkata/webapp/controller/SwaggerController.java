fc3b07c84df3a01ac815dcbec223c92b12b4e02e
OJIeLLlka <olbastnie@gmail.com>
2022-08-08 08:17:34 +0300

    Logged /user in SwaggerController

--- src/main/java/com/example/socialkata/webapp/controller/SwaggerController.java
+++ src/main/java/com/example/socialkata/webapp/controller/SwaggerController.java
@@ -1,11 +1,15 @@
 package com.example.socialkata.webapp.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SwaggerController {
+
+    private final Logger logger = LogManager.getLogger(SwaggerController.class); // configured by INFO level
 
     @RequestMapping(value = "/custom", method = RequestMethod.POST)
     public String custom() {
@@ -15,6 +19,10 @@ public class SwaggerController {
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String user() {
+        logger.info("Info Message");
+        logger.debug("Debug Message"); // not tracked
+        logger.warn("Warn message");
+        logger.error("Error message");
         return "user";
     }
 }
